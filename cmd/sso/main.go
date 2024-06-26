2545c68a13d7a17ea7869e04a17e41525bb105cd
Zrossiz <soon.pluxury@mail.ru>
2024-06-26 19:21:54 +0300

    chore: added graceful shutdown

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log/slog"
 	"os"
+	"os/signal"
 	"sso/internal/app"
 	"sso/internal/config"
 	"sso/internal/lib/logger/handlers/slogpretty"
+	"syscall"
 )
 
 const (
@@ -23,7 +25,18 @@ func main() {
 
 	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
 
-	application.GRPCSrv.MustRun()
+	go application.GRPCSrv.MustRun()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	sign := <-stop
+
+	log.Info("Stopping application", slog.String("signal", sign.String()))
+
+	application.GRPCSrv.Stop()
+
+	log.Info("Application stopped")
 }
 
 func setupLogger(env string) *slog.Logger {
--- internal/app/grpc/app.go
+++ internal/app/grpc/app.go
@@ -62,7 +62,7 @@ func (a *App) Stop() {
 	const op = "grpcapp.Stop"
 
 	a.log.With(slog.String("op", op)).
-		Info("stopping gRPC server", slog.Int("port", a.port))
+		Info("Stopping gRPC server")
 
 	a.gRPCServer.GracefulStop()
 }
