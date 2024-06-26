d8330fe087007227aa2b3692ada5bee8805e1e8e
Zrossiz <soon.pluxury@mail.ru>
2024-06-27 00:02:58 +0300

    chore: added service layer boilerplate

---
+++ internal/services/auth/auth.go
@@ -0,0 +1,71 @@
+package auth
+
+import (
+	"context"
+	"log/slog"
+	"sso/internal/domain/models"
+	"time"
+)
+
+type Auth struct {
+	log         *slog.Logger
+	usrSaver    UserSaver
+	usrProvider UserProvider
+	appProvider AppProvider
+	tokenTTL    time.Duration
+}
+
+type UserSaver interface {
+	SaveUser(
+		ctx context.Context,
+		email string,
+		passHash []byte,
+	) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int) (models.App, error)
+}
+
+func New(
+	log *slog.Logger,
+	userSaver UserSaver,
+	userProvider UserProvider,
+	appProvider AppProvider,
+	tokenTTL time.Duration,
+) *Auth {
+
+	return &Auth{
+		usrSaver:    userSaver,
+		usrProvider: userProvider,
+		log:         log,
+		appProvider: appProvider,
+		tokenTTL:    tokenTTL,
+	}
+}
+
+func (a *Auth) Login(
+	ctx context.Context,
+	email string,
+	password string,
+	appID int,
+) (string, error) {
+	panic("not implemented")
+}
+
+func (a *Auth) RegisterNewUser(
+	ctx context.Context,
+	email string,
+	pass string,
+) (int64, error) {
+	panic("not implemented")
+}
+
+func (a *Auth) IsAdmin(ctx context.Context, userID int64) (bool error) {
+	panic("not implemented")
+}
