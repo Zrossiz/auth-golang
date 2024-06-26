08c016574dd461305ca438777fe489d6bf8fd3ce
Zrossiz <soon.pluxury@mail.ru>
2024-06-26 18:01:45 +0300

    feat: added logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,12 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"sso/internal/config"
+	"sso/internal/lib/logger/handlers/slogpretty"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+	log.Info("Starting application", slog.Any("config", cfg))
+}
 
-	fmt.Println(cfg)
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelError}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+	return slog.New(handler)
 }
--- go.mod
+++ go.mod
@@ -5,9 +5,17 @@ go 1.21.3
 require github.com/ilyakaznacheev/cleanenv v1.5.0
 
 require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
+
+require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/Zrossiz/sso-proto v0.0.0-20240626133009-051ff95ec387 // indirect
+	github.com/fatih/color v1.17.0
 	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
